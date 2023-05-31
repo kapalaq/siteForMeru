363ce640d920de6b1e68e5e0182096187a7b6d18
kapalaq <mainmailformail@gmail.com>
2023-05-31 20:06:06 +0600

    New commit

---
+++ theGift/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for theGift project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'theGift.settings')
+
+application = get_asgi_application()
