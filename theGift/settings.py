c39a2034aebb1559446cd2d45ebbf535a75eca85
kapalaq <108875253+kapalaq@users.noreply.github.com>
2023-05-31 21:06:22 +0600

    Updated

--- theGift/settings.py
+++ theGift/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open("key.txt", 'r') as file:
+with open(BASE_DIR / "theGift/key.txt", 'r') as file:
     SECRET_KEY = file.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
