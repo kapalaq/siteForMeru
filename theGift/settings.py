f7de9415f53e4e5ddd09ce1b3fc5716b6174933d
kapalaq <108875253+kapalaq@users.noreply.github.com>
2023-05-31 21:19:47 +0600

    Update settings.py

--- theGift/settings.py
+++ theGift/settings.py
@@ -27,7 +27,9 @@ with open(BASE_DIR / "theGift/key.txt", 'r') as file:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['siteformeru-production.up.railway.app', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://siteformeru-production.up.railway.app']
 
 
 # Application definition
